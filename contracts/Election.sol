44ee1ab026e04103ad4a91096344821d6869c9bb
Zlash65 <zarrar65@gmail.com>
2018-12-07 16:23:16 +0530

    logic for casting vote added and emitting event added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -12,7 +12,14 @@ contract Election {
 	// mapping to candidates -> sort of an array
 	mapping(uint => Candidate) public candidates;
 
+	// store accounts that have voted
+	mapping(address => bool) public voters;
+
 	uint public candidatesCount;
+
+	event votedEvent(
+		uint indexed _candidateId
+	);
 
 	// constructor
 	constructor() public {
@@ -24,6 +31,24 @@ contract Election {
 	function addCandidate(string _name) private {
 		candidatesCount++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote(uint _candidateId) public {
+
+		// check if voter already voted
+		require(!voters[msg.sender]);
+
+		// check if the candidate is legit
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+		// record the account that is voting
+		voters[msg.sender] = true;
+
+		// update candidate vote count
+		candidates[_candidateId].voteCount++;
+
+		// trigger voted event
+		emit votedEvent(_candidateId);
 	}
 
 
