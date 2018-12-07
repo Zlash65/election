8668e8ec7a4f0985d263c0b8203064da89b9615f
Zlash65 <zarrar65@gmail.com>
2018-12-07 10:25:25 +0530

    adding candidates with a struct type

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,29 @@ pragma solidity ^0.4.24;
 
 contract Election {
 
-	string public candidate;
-
-	constructor(Election) public {
-		candidate = "Candidate 1";
+	// model a structure for Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
 	}
+
+	// mapping to candidates -> sort of an array
+	mapping(uint => Candidate) public candidates;
+
+	uint public candidatesCount;
+
+	// constructor
+	constructor() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	// adding candidate and updating count
+	function addCandidate(string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+
 }
